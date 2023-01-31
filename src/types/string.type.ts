536890d0a18cbe8a3e1db707e4be9abf1f3e1e79
unknown <chandler.baird21@gmail.com>
2023-01-31 14:50:04 -0700

    added string to types src folder

---
+++ src/types/string.type.ts
@@ -0,0 +1,11 @@
+let firstName: string = "Mark";
+
+// all string methods are allowed on string types
+
+let fullName: String = firstName + " " + "Doe";
+
+fullName.split(seperator: " ");
+
+// Math methods won't be accepted on String
+// fullName = 123456;
+// Math.log(fullName);
